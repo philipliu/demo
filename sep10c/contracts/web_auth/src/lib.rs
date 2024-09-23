08722b9293fe6e0b40ee9e86100ae15b0b61d4da
philipliu <philip.liu@stellar.org>
2024-09-23 16:41:03 -0400

    Rename "account" to "address"

--- sep10c/contracts/web_auth/src/lib.rs
+++ sep10c/contracts/web_auth/src/lib.rs
@@ -14,21 +14,12 @@ pub enum WebAuthError {
 #[contractimpl]
 impl WebAuthContract {
     pub fn web_auth_verify(env: Env, args: Map<String, String>) -> Result<(), WebAuthError> {
-        if let Some(account_id) = args.get(String::from_str(&env, "account")) {
+        if let Some(account_id) = args.get(String::from_str(&env, "address")) {
             let addr = Address::from_string(&account_id);
             addr.require_auth();
         } else {
             return Err(WebAuthError::WebAuthBadAddress);
         };
-
-        if let Some(client_domain_signing_key) =
-            args.get(String::from_str(&env, "client_domain_signing_key"))
-        {
-            // TODO: only allow ed25519 signers
-            let addr = Address::from_string(&client_domain_signing_key);
-            addr.require_auth();
-        }
-
         Ok(())
     }
 }
