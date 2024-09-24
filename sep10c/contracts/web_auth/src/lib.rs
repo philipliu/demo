d1cb0c494d27b22f761a2b42ac7b42eca578f763
philipliu <philip.liu@stellar.org>
2024-09-23 23:38:40 -0400

    Optional arguments

--- sep10c/contracts/web_auth/src/lib.rs
+++ sep10c/contracts/web_auth/src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Address, Env};
-use soroban_sdk::{contracterror, Map, String};
+use soroban_sdk::{contract, contracterror, contractimpl, Address, Env, String};
 
 #[contract]
 pub struct WebAuthContract;
@@ -8,19 +7,19 @@ pub struct WebAuthContract;
 #[contracterror]
 #[derive(Clone, Copy)]
 pub enum WebAuthError {
-    WebAuthBadAddress = 1,
+    BadAddress = 1,
 }
 
 #[contractimpl]
 impl WebAuthContract {
     pub fn web_auth_verify(
-        env: Env,
-        address: String,               // TODO: use Address
-        _memo: String,                 // IGNORED
-        _home_domain: String,          // IGNORED
-        _web_auth_domain: String,      // IGNORED
-        _client_domain: String,        // IGNORED
-        client_domain_address: String, // TODO: use Address
+        _env: Env,
+        address: String,
+        _memo: Option<String>,            // IGNORED
+        _home_domain: Option<String>,     // IGNORED
+        _web_auth_domain: Option<String>, // IGNORED
+        _client_domain: Option<String>,   // IGNORED
+        _client_domain_address: Option<String>,
     ) -> Result<(), WebAuthError> {
         let addr = Address::from_string(&address);
         addr.require_auth();
